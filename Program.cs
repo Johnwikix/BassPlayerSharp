b487a7f2bb0526bba2acfa552f3df1ae4f63f78c
pkc <906847109@qq.com>
2025-10-22 18:59:48 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using BassPlayerSharp.Service;
+using System.Runtime;
 
 public class Program
 {   
     public static async Task Main(string[] args)
-    {       
+    {
+        GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
         var tcpService = new TcpService();
         await tcpService.StartAsync();
-        Console.WriteLine("PipeService started");
     }
 }
